8ae272d50a5efcde4e3fde09008b3d31d2ecd7ac
Mike Lutton <mlutton@groupon.com>
2016-06-13 00:13:34 -0500

    Added initial payments controller and model

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,11 @@
+class PaymentsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: 'not_found', status: :not_found
+  end
+
+  def index
+    render json: Payment.all
+  end
+
+end
---
+++ spec/controllers/payments_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe PaymentsController, type: :controller do
+  describe '#index' do
+    it 'responds with a 200' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
